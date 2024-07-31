d7ede29aead82f5b1a9a13fc3f821953e41b6482
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-31 23:52:43 +0900

    feat: change project tree in packages/ui

--- packages/ui/src/button/button.tsx
+++
@@ -1,5 +0,0 @@
-type ButtonProps = object
-
-export const Button = ({}: ButtonProps) => {
-    return <div className="ui-p-5 ui-rounded-lg ui-w-fit ui-bg-emerald-300">Button Component</div>
-}
---
+++ packages/ui/src/components/button/button.tsx
@@ -0,0 +1,5 @@
+type ButtonProps = object
+
+export const Button = ({}: ButtonProps) => {
+    return <div className="ui-p-5 ui-rounded-xl ui-w-full ui-text-center ui-bg-indigo-300">Button Component</div>
+}
