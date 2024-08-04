315063d239a7a556f675c42c63bdaa70c7b2d4d0
tf63 <74246282+tf63@users.noreply.github.com>
2024-08-04 17:26:28 +0900

    feat: add unit test to packages/ui

---
+++ packages/ui/src/components/card/card.spec.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react'
+import { Card } from '.'
+
+test('renders Card component', () => {
+    render(<Card />)
+})
---
+++ packages/ui/vitest.config.ts
@@ -0,0 +1,38 @@
+import path from 'node:path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        include: ['./src/**/*.{test,spec}.{ts,tsx}'],
+        exclude: [
+            '**/src/test/e2e',
+            '**/node_modules/**',
+            '**/dist/**',
+            '**/.{idea,git,cache,output,temp,next}/**',
+            '**/*.stories.*',
+            '**/*.config.*',
+            '**/*.d.ts',
+        ],
+        coverage: {
+            enabled: true,
+            reportsDirectory: './src/test/unit/coverage',
+            reporter: ['text', 'json', 'html'],
+            exclude: [
+                '**/node_modules/**',
+                '**/dist/**',
+                '**/.{idea,git,cache,output,temp,next}/**',
+                '**/*.stories.*',
+                '**/*.config.*',
+                '**/*.d.ts',
+            ],
+        },
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: ['./src/test/unit/setup.ts'],
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+})
