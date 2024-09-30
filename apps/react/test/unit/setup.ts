ab47d50367b306fdd921adec5221f5f727b2264c
tf63 <74246282+tf63@users.noreply.github.com>
2024-09-30 23:26:34 +0900

    feat: add setup for portable story

--- apps/react/test/unit/setup.ts
+++ apps/react/test/unit/setup.ts
@@ -1,8 +1,15 @@
+import { setProjectAnnotations } from '@storybook/react'
+
 import '@testing-library/jest-dom/vitest'
 
+import previewAnnotations from '@repo/storybook/config/preview.tsx'
 import { cleanup } from '@testing-library/react'
-import { afterEach } from 'vitest'
+import { afterEach, beforeAll } from 'vitest'
+
+const annotations = setProjectAnnotations([{ ...previewAnnotations, decorators: [] }])
 
 afterEach(() => {
     cleanup()
 })
+
+beforeAll(annotations.beforeAll)
