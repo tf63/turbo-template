58c15fcd9b72a39ab8ec70c5b8d17afb8a26a811
tf63 <74246282+tf63@users.noreply.github.com>
2025-05-25 12:12:38 +0900

    feat: update storybook meta info

--- apps/next/src/features/client-component/client-component.stories.tsx
+++ apps/next/src/features/client-component/client-component.stories.tsx
@@ -1,15 +1,16 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
 import { ClientComponent } from './client-component'
 
 type ClientComponentType = typeof ClientComponent
 
-export default {
+const meta: Meta<ClientComponentType> = {
     title: 'ClientComponent',
     component: ClientComponent,
     render: (props) => <ClientComponent {...props} />,
-    decorators: [(Story: StoryFn) => <Story />],
-} satisfies Meta<ClientComponentType>
+    decorators: [(Story) => Story()],
+}
+export default meta
 
 export const Default: StoryObj<ClientComponentType> = {
     args: {},
--- packages/ui/src/components/not-violation-card/not-violation-card.stories.tsx
+++ packages/ui/src/components/not-violation-card/not-violation-card.stories.tsx
@@ -1,15 +1,16 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
 import { NotViolationCard } from './not-violation-card'
 
 type NotViolationCardType = typeof NotViolationCard
 
-export default {
+const meta: Meta<NotViolationCardType> = {
     title: 'NotViolationCard',
     component: NotViolationCard,
     render: (props) => <NotViolationCard {...props} />,
-    decorators: [(Story: StoryFn) => <Story />],
-} satisfies Meta<NotViolationCardType>
+    decorators: [(Story) => Story()],
+}
+export default meta
 
 export const Default: StoryObj<NotViolationCardType> = {
     args: { text: 'Not Violation' },
